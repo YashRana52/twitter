203ca622a2219376db1b379ecaa3d8a03b9f753b
Yash Kumar Rana <2200520100072@ietlucknow.ac.in>
2025-02-16 11:32:03 +0530

    constant chnage

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT = "process.env.URL/api/v1/user";
-export const TWEET_API_END_POINT = "process.env.URL/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-mi9y.onrender.com/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-mi9y.onrender.com/api/v1/tweet";
